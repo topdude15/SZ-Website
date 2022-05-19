aa073930b6110d578b0d5d4d30189d3c03555ea9
Trevor Rose <topdude15@gmail.com>
2022-05-18 17:51:47 -0700

    Moved JSON data to external file
    Moved JSON data for the menu to their own separate files for more flexibility.  Changed the images on each menu item to fit the container size so they don't disrupt the rest of the items if the image is not the correct size.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-var jsonData ='[{"name":"Sensations","imagePath":"img/ChocolateConduction.png","destination":"sensations.html","imageWidth":"70%","divId":"sensations"},{"name":"Create Your Own","imagePath":"img/PeanutButterBonds.png","destination":"create.html","imageWidth":"70%","divId":"create"},{"name":"Soda Lab","imagePath":"img/coke.png","destination":"sodalab.html","imageWidth":"45.75%","divId":"sodalab"},{"name":"Pies","imagePath":"img/TropicalTurbine.png","destination":"pie.html","imageWidth":"70%","divId":"pie"},{"name":"Specialty Items","imagePath":"img/MagneticMint.png","destination":"special.html","imageWidth":"70%","divId":"special"},{"name":"Specialty Items","imagePath":"img/MagneticMint.png","destination":"special.html","imageWidth":"70%","divId":"special"}]';
+function createData(jsonFile) {
+	$.getJSON(jsonFile, function(data) {
 
-var obj = JSON.parse( jsonData );
+    var tmp = '';
+		$.each(data, function(key, value) {
+			tmp += '<a href = "' + value.destination + '" class = "menuItem">';
+			tmp += '	<div class = "orderItem" id = "' + value.divId + '">';
+			tmp += '		<img src="' + value.imagePath + '" width = "' + value.imageWidth + '">';
+			tmp += '		<p>' + value.name + '</p>';
+			tmp += '</div>';
+			tmp += '</a>';
+		});
 
-var tmp = '';
-
-$.each(obj, function(key, value) {
-		tmp += '<a href = "' + value.destination + '" class = "menuItem">';
-		tmp += '	<div class = "orderItem" id = "' + value.divId + '">';
-		tmp += '		<img src="' + value.imagePath + '" width = "' + value.imageWidth + '">';
-		tmp += '		<p>' + value.name + '</p>';
-		tmp += '</div>';
-		tmp += '</a>';
+		$('#main').prepend(tmp);
 	});
-
-$('#main').prepend(tmp);
-
+}
 
 
+// The following contains the code for the nagivation menu when the screen is small enough to shrink into the mobile view
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 const items = document.querySelectorAll(".item");
