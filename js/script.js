d3690eef8884d6f8fff3ea0c5402dc4a597c614e
Trevor Rose <topdude15@gmail.com>
2022-05-20 14:12:17 -0700

    Changed file interaction behavior
    Navigation bar on page now loaded from external navbar.html file to reduce repeated code.  SensationInfo.html updated to get query information passed when a sensation is selected from the sensation menu.

--- js/script.js
+++ js/script.js
@@ -16,53 +16,15 @@ function createData(jsonFile) {
 }
 
 
-// The following contains the code for the nagivation menu when the screen is small enough to shrink into the mobile view
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
-const items = document.querySelectorAll(".item");
+function loadSensation(sensId, jsonFile) {
+  console.log("Loading sensation with ID of " + sensId);
 
-/* Toggle mobile menu */
-function toggleMenu() {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
-    toggle.querySelector("a").innerHTML = "Menu";
-  } else {
-    menu.classList.add("active");
-    toggle.querySelector("a").innerHTML = "Close Menu";
-  }
+  $.getJSON(jsonFile, function(data) {
+    var obj = data.find(o => o.id === 1);
+    createSensationInfo(obj);
+  });
 }
 
-/* Activate Submenu */
-function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
-  } else {
-    this.classList.add("submenu-active");
-  }
+function createSensationInfo(sensData) {
+  console.log(sensData["name"]);
 }
-
-/* Close Submenu From Anywhere */
-function closeSubmenu(e) {
-  if (menu.querySelector(".submenu-active")) {
-    let isClickInside = menu
-      .querySelector(".submenu-active")
-      .contains(e.target);
-
-    if (!isClickInside && menu.querySelector(".submenu-active")) {
-      menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    }
-  }
-}
-/* Event Listeners */
-toggle.addEventListener("click", toggleMenu, false);
-for (let item of items) {
-  if (item.querySelector(".submenu")) {
-    item.addEventListener("click", toggleItem, false);
-  }
-  item.addEventListener("keypress", toggleItem, false);
-}
-document.addEventListener("click", closeSubmenu, false);
-
