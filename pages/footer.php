493e427b5ed3f3471e8865da04411794356d74ec
Trevor Rose <topdude15@gmail.com>
2022-11-27 23:58:58 -0700

    Added content to footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,10 +1,38 @@
-<p>© Copyright 2022 Sub Zero Ice Cream</p>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<footer>
+	<div id = "footer-sections">
+		<div class = "footer-section">
+			<h3>Be The Boss</h3>
+			<a href="franchise.php">Own a Franchise</a>
+			<a href="franchise.php">Franchising FAQ</a>
+			<a href="franchise.php">Ownership</a>
+			<a href="franchise.php">Contact Us</a>
+			<a href="franchise.php">Apply</a>
+		</div>
+		<div class = "footer-section">
+			<h3>Menu</h3>
+			<a href="franchise.php">Our Process</a>
+			<a href="franchise.php">Ice Cream</a>
+			<a href="franchise.php">Cakes and Pies</a>
+			<a href="franchise.php">Soda Lab</a>
+			<a href="franchise.php">Nutritional Information</a>
+		</div>
+		<div class = "footer-section">
+			<h3>Community</h3>
+			<a href="franchise.php">Catering</a>
+			<a href="franchise.php">STEM Education</a>
+			<a href="franchise.php">Fun Facts of Nitrogen</a>
+			<a href="franchise.php">Rewards Program</a>
+		</div>
+		<div class = "footer-section">
+			<h3>Getting to Know Us</h3>
+			<a href="franchise.php">Shark Tank</a>
+			<a href="franchise.php">Careers - Job Application</a>
+			<a href="franchise.php">Our Story</a>
+		</div>
+		<div class = "footer-section">
+			<h3>Follow Us</h3>
+			<a href="franchise.php">Franchising</a>
+			<a href="franchise.php">Franchising</a>
+		</div>
+	</div>
+</footer>
