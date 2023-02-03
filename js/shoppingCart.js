433d3bf9e28390dd3493b464ec2c59e8af5f7155
Trevor Rose <topdude15@gmail.com>
2023-02-03 14:31:12 -0700

    Added creation for create your own
    Must be changed from current iteration which iterates over each possible sensation, creating 12 items for create your own for each one item in the cart

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -15,6 +15,7 @@ function loadShoppingCart() {
   $.getJSON("../data/sensations.json", function(data) {
     var obj = data.find(function(sensation, index) {
       for (i = 0; i < currentOrder.orderItems.length; i++) {
+        console.log(currentOrder.orderItems.length);
         if (currentOrder.orderItems[i].itemType == "sensation") {
           if (sensation.id == currentOrder.orderItems[i].sensationId) {
             tmp += '<div class = "cartItem">';
@@ -25,6 +26,16 @@ function loadShoppingCart() {
             tmp += '<p class = "cartSubtitle">Waffle included: ' + (currentOrder.orderItems[i].includeWaffle ? "Yes" : "No") + '</p>';
             tmp += '</div>';
           }
+        } else if (currentOrder.orderItems[i].itemType == "create") {
+          tmp += '<div class = "cartItem">';
+          tmp += '<img class = "cartImg src = "../img/sensation/BananaCreamPi.png">';
+          tmp += '<p class = "cartTitle">Create Your Own</p>';
+          tmp += '<p class = "cartSubtitle">Size: ' + currentOrder.orderItems[i].itemSize + '</p>';
+          tmp += '<p class = "cartSubtitle">Base: ' + currentOrder.orderItems[i].itemBase + '</p>';
+          tmp += '<p class = "cartSubtitle">Waffle Included: ' + (currentOrder.orderItems[i].includeWaffle ? "Yes" : "No") + '</p>';
+          tmp += '<p class = "cartSubtitle">Mixins: ';
+          tmp += '<p class = "cartSubtitle">Flavors: ';
+          tmp += '</div>';
         }
       };
     })
