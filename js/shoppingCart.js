b79361d837ace727e730c1e431f91bd0cc893e55
Trevor Rose <topdude15@gmail.com>
2023-02-03 22:54:18 -0700

    Flavors and Mixins appear on cart screen

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -12,35 +12,46 @@ function loadShoppingCart() {
   console.log(currentOrder);
   var tmp = '';
   var sens = '';
-  $.getJSON("../data/sensations.json", function(data) {
-    var obj = data.find(function(sensation, index) {
-      for (i = 0; i < currentOrder.orderItems.length; i++) {
-        console.log(currentOrder.orderItems.length);
-        if (currentOrder.orderItems[i].itemType == "sensation") {
-          if (sensation.id == currentOrder.orderItems[i].sensationId) {
-            tmp += '<div class = "cartItem">';
-            tmp += '<img class = "cartImg" src = "../' + sensation.imagePath + '">';
-            tmp += '<p class = "cartTitle">' + sensation.name + '</p>';
-            tmp += '<p class = "cartSubtitle">Size: ' + currentOrder.orderItems[i].itemSize + '</p>';
-            tmp += '<p class = "cartSubtitle">Base: ' + currentOrder.orderItems[i].itemBase + '</p>';
-            tmp += '<p class = "cartSubtitle">Waffle included: ' + (currentOrder.orderItems[i].includeWaffle ? "Yes" : "No") + '</p>';
-            tmp += '</div>';
-          }
-        } else if (currentOrder.orderItems[i].itemType == "create") {
-          tmp += '<div class = "cartItem">';
-          tmp += '<img class = "cartImg src = "../img/sensation/BananaCreamPi.png">';
-          tmp += '<p class = "cartTitle">Create Your Own</p>';
-          tmp += '<p class = "cartSubtitle">Size: ' + currentOrder.orderItems[i].itemSize + '</p>';
-          tmp += '<p class = "cartSubtitle">Base: ' + currentOrder.orderItems[i].itemBase + '</p>';
-          tmp += '<p class = "cartSubtitle">Waffle Included: ' + (currentOrder.orderItems[i].includeWaffle ? "Yes" : "No") + '</p>';
-          tmp += '<p class = "cartSubtitle">Mixins: ';
-          tmp += '<p class = "cartSubtitle">Flavors: ';
-          tmp += '</div>';
-        }
-      };
-    })
-      $('#main').append(tmp);
-  })
+  for(i = 0; i < currentOrder.orderItems.length; i++) {
+    console.log(currentOrder.orderItems[i]);
+    if (currentOrder.orderItems[i].itemType == "sensation") {
+
+    } else if (currentOrder.orderItems[i].itemType == "create") {
+      let formattedMixins = (currentOrder.orderItems[i].mixins).join(', ');
+      let formattedFlavors = (currentOrder.orderItems[i].flavors).join(', ');
+      tmp += '<div class = "cartItem">';
+      tmp += '<img class = "cartImg src = "../img/sensation/BananaCreamPi.png">';
+      tmp += '<p class = "cartTitle">Create Your Own</p>';
+      tmp += '<p class = "cartSubtitle">Size: ' + currentOrder.orderItems[i].itemSize + '</p>';
+      tmp += '<p class = "cartSubtitle">Base: ' + currentOrder.orderItems[i].itemBase + '</p>';
+      tmp += '<p class = "cartSubtitle">Waffle Included: ' + (currentOrder.orderItems[i].includeWaffle ? "Yes" : "No") + '</p>';
+      tmp += '<p class = "cartSubtitle">Flavors: ' + formattedFlavors;
+      tmp += '<p class = "cartSubtitle">Mixins: ' + formattedMixins;
+      tmp += '</div>';
+    }
+  }
+  // $.getJSON("../data/sensations.json", function(data) {
+  //   var obj = data.find(function(sensation, index) {
+  //     for (i = 0; i < currentOrder.orderItems.length; i++) {
+  //       console.log(currentOrder.orderItems.length);
+  //       if (currentOrder.orderItems[i].itemType == "sensation") {
+  //         if (sensation.id == currentOrder.orderItems[i].sensationId) {
+  //           tmp += '<div class = "cartItem">';
+  //           tmp += '<img class = "cartImg" src = "../' + sensation.imagePath + '">';
+  //           tmp += '<p class = "cartTitle">' + sensation.name + '</p>';
+  //           tmp += '<p class = "cartSubtitle">Size: ' + currentOrder.orderItems[i].itemSize + '</p>';
+  //           tmp += '<p class = "cartSubtitle">Base: ' + currentOrder.orderItems[i].itemBase + '</p>';
+  //           tmp += '<p class = "cartSubtitle">Waffle included: ' + (currentOrder.orderItems[i].includeWaffle ? "Yes" : "No") + '</p>';
+  //           tmp += '</div>';
+  //         }
+  //       } else if (currentOrder.orderItems[i].itemType == "create") {
+
+  //       }
+  //     };
+  //   })
+  //   $('#main').append(tmp);
+  // })
+  $('#main').append(tmp);
 }
 
 function calculatePricing() {
